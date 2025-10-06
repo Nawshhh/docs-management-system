2b17ffe03fde4183ede73e4e97dcda064df0c79a
Nawshhh <wilbertshawnvillagarcia@gmail.com>
2025-10-06 22:20:48 +0800

    fixed issues of config file

--- backend/app/config.py
+++ backend/app/config.py
@@ -9,7 +9,9 @@ class Settings(BaseSettings):
     MONGO_DB: str = os.getenv("MONGO_DB")
     JWT_SECRET: str = os.getenv("JWT_SECRET")
     JWT_REFRESH_SECRET: str = os.getenv("JWT_REFRESH_SECRET")
-    ALLOWED_ORIGINS: list[str] = ["http://localhost:5173"]
+    ALLOWED_ORIGINS: list[str] = (
+        os.getenv("ALLOWED_ORIGINS", "http://localhost:5173").split(",")
+    )
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
