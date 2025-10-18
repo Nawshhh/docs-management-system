b402a4b4012f90504b3cb0a770459cdb623b672b
Nawshhh <wilbertshawnvillagarcia@gmail.com>
2025-10-18 15:19:27 +0800

    documents options added

--- frontend/src/pages/Documents.tsx
+++ frontend/src/pages/Documents.tsx
@@ -13,6 +13,20 @@ function Documents() {
   return (
     <div className='w-screen h-screen flex flex-col items-center justify-center bg-zinc-900 px-20 md:px-80 sm:px-10'>
         <p className='font-semibold text-3xl text-gray-200 mb-10'>Documents Page</p>
+        <div className='grid grid-cols-3 gap-6 w-full my-10 text-gray-200 text-xl font-semibold'>
+            <button 
+                className='flex items-center justify-center bg-slate-700 hover:bg-slate-600 rounded-md h-20 cursor-pointer'>
+                View Documents
+            </button>
+            <button  
+                className='flex items-center justify-center bg-slate-700 hover:bg-slate-600 rounded-md h-20 cursor-pointer'>
+                Approve Documents
+            </button>
+            <button 
+                className='flex items-center justify-center bg-slate-700 hover:bg-slate-600 rounded-md h-20 cursor-pointer'>
+                Upload Documents
+            </button>
+        </div>
         <button 
             onClick={handleHome}
             className='mt-5 h-8 px-10 bg-neutral-700 hover:bg-neutral-600 rounded-md text-gray-200 hover:text-gray-300 text-sm cursor-pointer'
